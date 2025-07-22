bc7644b36263efef6d4153b48b93ff31fff148fd
EduPopez <eduardo.lozu@gmail.com>
2025-07-22 12:51:12 -0400

    BE-OpenAPI Swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce BE APIs",
+        default_version = "v1",
+        description = "This is the documentation for E-commerce BE APIs",
+        terms_of_service = "https://misitio.dominio.com",
+        contact= openapi.Contact(email="macareno@macasnas.com"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,  
+    permission_classes=(permissions.AllowAny, )
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+
+    # Documentación
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
